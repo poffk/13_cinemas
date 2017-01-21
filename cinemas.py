a1c4335e2ce37bff649da21a115cc68c086680ef
Alexander <Alexander>
2017-01-21 15:57:36 +0300

    Название функции arthouse_check изменено. Функция fetch_movie_info разделена на две функции (+parse_kinopoisk). Функция parse_kinopoisk возвращает значение типа dict.

--- cinemas.py
+++ cinemas.py
@@ -18,34 +18,42 @@ def parse_afisha_list(raw_html):
     soup = bs4.BeautifulSoup(raw_html, 'html.parser')
     movies = []
     for film in soup.find_all('div', {'class': 'object s-votes-hover-area collapsed'}):
-        if not arthouse_check(film):
+        if not is_arthouse(film):
             movies.append((film.find('div', {'class': "m-disp-table"})).find('a').text)
     return movies[:TOP]
 
 
-def arthouse_check(soup_film):
+def is_arthouse(soup_film):
     return len(soup_film.find_all('tr')) < ARTHOUSE_CINEMAS_NUMBER
 
 
 def fetch_movie_info(movie_title):
     payload = {'first': 'yes', 'kp_query': '{}'.format(movie_title)}
     request_to_kinopoisk = requests.get(KINOPOISK_SEARCH_URL, params=payload).content
-    soup = bs4.BeautifulSoup(request_to_kinopoisk, 'html.parser')
+    return request_to_kinopoisk
+
+
+def parse_kinopoisk(raw_html, movie_title):
+    soup = bs4.BeautifulSoup(raw_html, 'html.parser')
     average_rating = soup.find('span', {'class': 'rating_ball'}).text
     voters_amount = soup.find('span', {'class': 'ratingCount'}).text
-    return (movie_title, float(average_rating), voters_amount)
+    return {'movie_name': movie_title,
+            'movie_rating': float(average_rating),
+            'voters_amount': voters_amount}
 
 
 def output_movies_to_console(movies):
     top_rating_films = sorted(movies, key=lambda movie: movie[1], reverse=True)
     for movie in top_rating_films[:TOP]:
-        print('Фильм {} имеет {} голосов со средней оценкой {}'.format(movie[0], movie[2], movie[1]))
+        print('Фильм {} имеет {} голосов со средней оценкой {}' \
+              .format(movie['movie_name'], movie['movie_rating'], movie['voters_amount']))
 
 
 if __name__ == '__main__':
     movies = parse_afisha_list(fetch_afisha_page())
     movies_info = []
     for movie in movies:
-        movies_info.append(fetch_movie_info(movie))
+        kinopoisk_raw_html = fetch_movie_info(movie)
+        movies_info.append(pasre_kinopoisk(kinopoisk_raw_html, movie))
         time.sleep(TIME_OF_TIMEOUT)
     output_movies_to_console(movies_info)
